469b7095270e72a4fad542525571c4fa0bc6bffc
bodason <tbo@systemate.dk>
2020-02-22 17:52:51 +0100

    added cars-data-handler service

--- src/app/services/cars-data-handler.service.ts
+++ src/app/services/cars-data-handler.service.ts
@@ -1,3 +1,6 @@
+import { YearBarGraph } from './year-graph';
+import { BaseBarGraph } from './base-bar-graph';
+import { BrandBarGraph } from './brand-graph';
 import { Injectable } from '@angular/core';
 import { HttpClient} from '@angular/common/http';
 import { Car } from './car';
@@ -7,15 +10,76 @@ import { Car } from './car';
 })
 export class CarsDataHandlerService {
   private Cars: Car[];
+  private BrandGraph = new BrandBarGraph();
+  private YearGraph = new YearBarGraph();
+
 
   constructor(private http: HttpClient) {
   }
 
-  async loadCars() {
+  public async loadCars() {
     if (!this.Cars) {
-      this.Cars = await this.http.get<Promise<Car[]>>('../../assets/Data/carsData.json').toPromise();
-      console.log(this.Cars);
+      const timerStart = new Date();
+
+      this.Cars = await this.http.get<Promise<Car[]>>('../../assets/Data/carsData.json').toPromise(); // TODO: Load from a server instead !
+      const timeElapsed = new Date().getTime() - timerStart.getTime(); // TODO: get rid of timer after perfomance measures
+
+      console.debug(`Load cars completed after ${timeElapsed / 1000} seconds`);
     }
+  }
+
+  public PrepareBrandGraph() {
+    const brandsAxis: string[] = [];
+    const consumtionAxis: string[] = [];
+
+    if (this.BrandGraph?.data == null) {
+      const timerStart = new Date();
+
+      this.Cars.forEach(car => {
+        brandsAxis.push(car.brand);
+        consumtionAxis.push(car.price);
+      });
+
+      this.BrandGraph.data = [{
+        type: 'bar',
+        x: consumtionAxis,
+        y: brandsAxis,
+        orientation: 'h'
+      }];
+      const timeElapsed = new Date().getTime() - timerStart.getTime(); // TODO: get rid of timer after perfomance measures
+      console.debug(`Prepare brand graph completed after ${timeElapsed / 1000} seconds`);
+    }
+  }
+
+  public GetBrandGraph(): BaseBarGraph {
+    return this.BrandGraph;
+  }
+
+  PrepareYearsGraphIfNull() {
+    const yearsAxis: string[] = [];
+    const consumtionAxis: string[] = [];
+
+    if (this.YearGraph?.data == null) {
+      const timerStart = new Date();
+
+      this.Cars.forEach(car => {
+        yearsAxis.push(car.year);
+        consumtionAxis.push(car.price);
+      });
+
+      this.YearGraph.data = [{
+        type: 'bar',
+        x: consumtionAxis,
+        y: yearsAxis,
+        orientation: 'h'
+      }];
+      const timeElapsed = new Date().getTime() - timerStart.getTime(); // TODO: get rid of timer after perfomance measures
+      console.debug(`Prepare brand graph completed after ${timeElapsed / 1000} seconds`);
+    }
+  }
+
+  public GetYearGraph(): BaseBarGraph {
+    return this.YearGraph;
   }
 
 }

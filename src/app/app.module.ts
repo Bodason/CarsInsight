373855a10ca0d791499c3c252bc4548b72093f4d
bodason <tbo@systemate.dk>
2020-02-22 17:51:28 +0100

    initial logic for brand-slicing-area added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { TopNavBarComponent } from './components/top-nav-bar/top-nav-bar.component';
 import { HttpClientModule } from '@angular/common/http';
+import { BrandSlicingAreaComponent } from './components/brand-slicing-area/brand-slicing-area.component';
+
+
+import * as PlotlyJS from 'plotly.js/dist/plotly.js';
+import { PlotlyModule, PlotlyViaCDNModule } from 'angular-plotly.js';
+
+PlotlyModule.plotlyjs = PlotlyJS;
 
 @NgModule({
   declarations: [
     AppComponent,
     HomePageComponent,
-    TopNavBarComponent
+    TopNavBarComponent,
+    BrandSlicingAreaComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    CommonModule,
+    PlotlyModule,
+    PlotlyViaCDNModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/brand-slicing-area/brand-slicing-area.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-slicing-area',
+  templateUrl: './brand-slicing-area.component.html',
+  styleUrls: ['./brand-slicing-area.component.scss']
+})
+export class BrandSlicingAreaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
